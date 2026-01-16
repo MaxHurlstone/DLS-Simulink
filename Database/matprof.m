18fb670d5745b5dbb134febb5eab8160efb2e357
Max Hurlstone <Max.Hurlstone@diamond.ac.uk>
2026-01-16 10:00:32 +0000

    Added mechanical.oscillating_translational_reference

--- Database/matprof.m
+++ Database/matprof.m
@@ -1,15 +1,13 @@
 function [Tinfo, Tdata] = matprof(matname)
-%MATPROFILE Prints short profile of input material
-%   The user inputs a material name according to one listed in the
-%   database. Max Hurlstone 12-2025
+%MATPROF Prints profile of chosen material
+%   Max Hurlstone 12-2025
+
 arguments (Input)
     matname (1,1) string   
 end
 
 matspec = split(matname,"_");
-
-
-matpath = "./" + matspec(1) + "/" + matname + ".csv";
+matpath = pwd + "\Materials\" + matspec(1) + "\" + matname + ".csv";
 
 opts = delimitedTextImportOptions;
 opts.DataLines = [1 6];
@@ -19,11 +17,17 @@ opts.DataLines = 6;
 opts.VariableNames = {'T', 'k'};
 Tdata = readtable(matpath,opts);
 
-% Loop through data and plot
-figure()
 x = Tdata{:,1};
+x = cell2mat(cellfun(@(x) str2double(x), x, 'UniformOutput', false));
 Y = table2array(Tdata(:,2:end));
+Y = cell2mat(cellfun(@(x) str2double(x), Y, 'UniformOutput', false));
 
+figure()
 plot(x,Y)
+grid on
+grid minor
+xlabel('T [K]')
+ylabel('k [W/mK]')
+title(matname,'Interpreter','latex')
 
 end
