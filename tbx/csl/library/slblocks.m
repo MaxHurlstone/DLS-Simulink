3f7f976ba67a0e0fbd4ee03f03666742c34619c0
Max Hurlstone <Max.Hurlstone@diamond.ac.uk>
2026-01-26 15:39:37 +0000

    changed slblock.m to point to library, might be why it wasn't visible

--- tbx/csl/library/slblocks.m
+++ tbx/csl/library/slblocks.m
@@ -1,5 +1,5 @@
 % Function to control how library appears in browser
 function blkStruct = slblocks
-Browser.Library = "DLS_Simscape_lib";
+Browser.Library = "DLS_Simscape";
 Browser.Name = "Simscape DLS";
 blkStruct.Browser = Browser;
