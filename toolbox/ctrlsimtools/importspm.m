564879acb6057d5e86c91c1157d6f2589a200b04
Max Hurlstone <Max.Hurlstone@diamond.ac.uk>
2026-03-20 11:16:52 +0000

    added new improved importspm() function to replace the old one

---
+++ toolbox/ctrlsimtools/importspm.m
@@ -0,0 +1,86 @@
+function importspm()
+%IMPORTSPM Import spm file into a MATLAB state space
+%   New (and hopefully improved) function that allows a user to import an
+%   spm file into a state space in their base workspace.
+
+%% Load file
+[file,location] = uigetfile('.spm');
+
+linarr = strip(readlines(fullfile(location,file)));
+
+%% Identify sections of spm data
+% Find locations of input/output labels
+idxi = find(strcmp("INPUT LABELS", linarr));
+idxo = find(strcmp("OUTPUT LABELS", linarr));
+
+% Find locations of A,B,C,D matrices
+idxa = find(strcmp("A MATRIX", linarr));
+idxb = find(strcmp("B MATRIX", linarr));
+idxc = find(strcmp("C MATRIX", linarr));
+idxd = find(strcmp("D MATRIX", linarr));
+
+%% Extract data
+% Extract A matrix data
+dims = sscanf(linarr(idxa+1),'%f');
+n = dims(1);
+A = zeros(n,n);
+
+count_A = dims(3);
+for i = 1:count_A
+    entry = sscanf(linarr(idxa+1+i),'%f');
+    A(entry(1),entry(2)) = entry(3);
+end
+
+% Extract B matrix data
+dims = sscanf(linarr(idxb+1),'%f');
+r = dims(2);
+B = zeros(n,r);
+
+count_B = dims(3);
+for i = 1:count_B
+    entry = sscanf(linarr(idxb+1+i),'%f');
+    B(entry(1),entry(2)) = entry(3);
+end
+
+% Extract C matrix data
+dims = sscanf(linarr(idxc+1),'%f');
+m = dims(1);
+C = zeros(m,n);
+
+count_C = dims(3); 
+for i = 1:count_C
+    entry = sscanf(linarr(idxc+1+i),'%f');
+    C(entry(1),entry(2)) = entry(3);
+end
+
+% Extract D matrix data
+dims = sscanf(linarr(idxd+1),'%f');
+m = dims(1);
+D = zeros(m,r); 
+
+count_D = dims(3); 
+for i = 1:count_D
+    entry = sscanf(linarr(idxd+1+i),'%f');
+    D(entry(1),entry(2)) = entry(3);
+end
+
+% Extract input label data
+u = linarr(idxi+1:idxi+r);
+
+% Extract output label data
+p = m/3;
+ids = repmat(["_DISP" "_VEL" "_ACC"]',p,1);
+outputs = repelem(linarr(idxo+1:idxo+p),3);
+
+y = strcat(outputs,ids);
+
+%% Create state space 
+% Create state space
+system = ss(A,B,C,D);
+system.u = u;
+system.y = y;
+
+% Assign to workspace
+assignin('base','system',system)
+
+end
