beec60aeb0a5c1f1e1cc37719915141877ae6a09
Max Hurlstone <Max.Hurlstone@diamond.ac.uk>
2026-03-25 07:52:35 +0000

    changed ToolboxMatlabPath to relative to try and fix installation issues

--- buildfile.m
+++ buildfile.m
@@ -41,7 +41,7 @@ opts.MinimumMatlabRelease = "R2024a";
 opts.ToolboxImageFile = "./images/dlsimulink.png";
 
 % Set up what should be on the path
-toAddToPath = genpath( fullfile(projectRoot, "toolbox") );
+toAddToPath = genpath( fullfile("toolbox") ); % genpath( fullfile(projectRoot, "toolbox") );
 toAddToPath = string( split(toAddToPath(1:end-1), ";") );
 opts.ToolboxMatlabPath = toAddToPath;
 
