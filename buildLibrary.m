a6ef2278708f7a6077df43ecec5d560eb981211c
Max Hurlstone <Max.Hurlstone@diamond.ac.uk>
2026-06-22 10:56:48 +0100

    changed buildLIbrary so that it now combines SSC files into the existing, manually constructed main library. I have now removed the other previous separate Simulink and Simscape libraries.

--- buildLibrary.m
+++ buildLibrary.m
@@ -11,11 +11,36 @@ tidyUp = onCleanup(@() cd(currFolder));
 % Save current open project root
 projRoot = currentProject().RootFolder;
 
-% CD to folder containing custom components
+% CD and build the SSC library
+% DO NOT CHANGE NAME (SSClib) OF THIS INTERMEDIATE LIBRARY
+sscLibPath = fullfile(projRoot, "toolbox", "dlsimulink", "csl", "library", "SSClib");
+
+% CD into SSC raw code folder and generate library
 cd(fullfile(projRoot, "toolbox", "dlsimulink", "csl", "components"));
+sscbuild("DLSimscape", "-output", sscLibPath);
+cd(fullfile(projRoot));
 
-% Create full path of library file
-libraryFileName = fullfile(projRoot, "toolbox", "dlsimulink", "csl", "simscape-library", "DLSimscape");
+% Load and find main subsystem in generated SSC library
+load_system(sscLibPath);
 
-% Build library
-sscbuild("DLSimscape", "-output", libraryFileName);
+source = find_system(sscLibName,"SearchDepth", 1,"Type", "Block");
+source = string(source);
+
+subSysName = extractAfter(source, "/");
+subSysName = string(subSysName);
+
+% Load working main library and insert SSC library
+dlsLibName = "DLSlib";
+
+load_system(dlsLibName)
+set_param(dlsLibName,"Lock","off");
+
+destination = join([dlsLibName, subSysName], "/");
+add_block(source, destination, "MakeNameUnique", "on",...
+          "CopyOption", "nolink");
+
+% Save and lock
+save_system(dlsLibName);
+
+% Delete intermediate SSC library
+delete SSClib.slx
--- toolbox/dlsimulink/csl/simscape-library/slblocks.m
+++
@@ -1,5 +0,0 @@
-% Function to control how library appears in browser
-function blkStruct = slblocks
-Browser.Library = "DLSimscape";
-Browser.Name = "Simscape DLS";
-blkStruct.Browser = Browser;
