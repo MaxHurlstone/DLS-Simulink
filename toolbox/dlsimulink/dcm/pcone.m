309789c7656ff8df7938227b8f2ac867c22a2abf
Max Hurlstone <Max.Hurlstone@diamond.ac.uk>
2026-07-14 12:34:14 +0100

    added docstring

--- toolbox/dlsimulink/dcm/pcone.m
+++ toolbox/dlsimulink/dcm/pcone.m
@@ -1,6 +1,21 @@
-function [D] = pcone(scoords,d,t,theta,xdim)
-%PCONE Summary of this function goes here
-%   Detailed explanation goes here
+function [pperc] = pcone(scoords,d,t,theta,xdim)
+%PCONE Visualise contact pressure for DCM xtal clamping
+%   Visualise the contact pressure for a DCM xtal heat exchanger clamping
+%   in order to determine the percentage of the contact surface that is
+%   actually clamped. This can feed into thermal calculations/simulations
+%   where the effective contact area is required.
+
+% Inputs:
+%   scoords - screw coordinates, double (:,2)
+%   d - screw head diameter, double
+%   t - clamping block/heat exchanger thickness, double
+%   theta - pressure cone angle, double
+%   xdim - clamping block/heat exchanger/xtal dimensions, double (1,2)
+
+% Outputs:
+%   pperc - percentage of overall area fully clamped, double
+%
+%   DLSimulink Toolbox
 
 arguments (Input)
     scoords (:,2) double
@@ -9,6 +24,10 @@ arguments (Input)
     theta (1,1) double = 30
     xdim (1,2) double = [60 40]
 end 
+
+arguments (Output)
+    pperc (1,1) double
+end
 
 % Calculate individual pressure contact diameter
 D = d + 2*t*tand(theta);
@@ -41,10 +60,13 @@ ylabel('y [mm]')
 xarea = area(xpatch);
 parea = xarea - area(nop);
 
+% Calcalate contact percentage
+pperc = 100*parea/xarea;
+
 fprintf('Xtal area = %0.0f mm2\n',xarea)
 fprintf('Pressure area = %0.0f mm2\n',parea)
-fprintf('Pressure area percent = %0.0f %%\n',100*parea/xarea)
+fprintf('Pressure area percent = %0.0f %%\n',pperc)
 
-title(sprintf('Pressure area percent = %0.0f %%\n',100*parea/xarea))
+title(sprintf('Pressure area percent = %0.0f %%\n',pperc))
 
 end
