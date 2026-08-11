d2d08309de31fa35191a428cd2a7b32ea1723ebd
MaxHurlstone <45340541+MaxHurlstone@users.noreply.github.com>
2026-08-11 10:14:17 +0100

    Features/sllibrary (#11)
    * initial commit. Added new Simulink library, with slblocks function. Now going to push to remote so that David can add his cryocooler model
    
    * initial commit. Added new Simulink library, with slblocks function. Now going to push to remote so that David can add his cryocooler model
    
    * DCT - Added Cryocooler subsystem model and Thermal contact resistance mask
    
    * DCT - Fixed Thermal Contact resistance mask errors
    
    * moved simulink block library into same folder as simscape code library, and updated buildLibrary.m
    
    * added a top level library data dictionary, as well as a cryocooler specific data dictionary, with an example variable in it
    
    * changed buildLIbrary so that it now combines SSC files into the existing, manually constructed main library. I have now removed the other previous separate Simulink and Simscape libraries.
    
    * adjusted code to delete SSClib on completion, and also delete any existing Simscape blocks before readding it
    
    * final tweaks to library building, and toolbox building code
    
    * updated contents.m
    
    ---------
    
    Co-authored-by: Tillin <david.tillin@diamond.ac.uk>

--- toolbox/dlsimulink/csl/library/slblocks.m
+++ toolbox/dlsimulink/csl/library/slblocks.m
@@ -1,5 +1,5 @@
 % Function to control how library appears in browser
 function blkStruct = slblocks
-Browser.Library = "DLSimscape";
-Browser.Name = "Simscape DLS";
+Browser.Library = "DLSlib";
+Browser.Name = "DLS Library";
 blkStruct.Browser = Browser;
