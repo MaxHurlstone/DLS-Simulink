e18fa8b9f0a98c9d2db01c8ece6532b061a5d02d
Max Hurlstone <Max.Hurlstone@diamond.ac.uk>
2026-09-24 11:15:00 +0100

    added docstring to exheat()

--- toolbox/dlsimulink/vaccs/exheat.m
+++ toolbox/dlsimulink/vaccs/exheat.m
@@ -1,15 +1,28 @@
 function exheat(Tout,Tsat,Cp,hfg,mdotmax)
-%EXHEAT Summary of this function goes here
-%   Detailed explanation goes here
-% arguments (Input)
-%     inputArg1
-%     inputArg2
-% end
-% 
-% arguments (Output)
-%     outputArg1
-%     outputArg2
-% end
+%EXHEAT Calculate power required to vaporise and heat a fluid
+%   Calculate the power required to vaporise and heat a fluid. This
+%   function displays a colourmap that shows the required power over
+%   various mass flow rates and inlet vapour qualities. This function was
+%   originally intended for VACCS development.
+%
+%   Inputs:
+%   Tout    - Heater outlet temperature, double
+%   Tsat    - Fluid saturation temperature, double
+%   Cp      - Fluid specific heat capacity, double
+%   hfg     - Fluid latent heat of vaporisation, double
+%   mdotmax - Maximum analysis mass flow rate, double
+%
+%   See also SHAH, PLOSS
+%
+%   DLSimulink Toolbox
+
+arguments (Input)
+    Tout    (1,1) double
+    Tsat    (1,1) double
+    Cp      (1,1) double
+    hfg     (1,1) double
+    mdotmax (1,1) double
+end
 
 % Create analysis grid
 x = 0:0.01:1;
